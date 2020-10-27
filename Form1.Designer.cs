e6d5162906d79f102bb169d0e2ef27d54c1af36f
Gabriel52 <gabrielbrune52@gmail.com>
2020-10-26 22:05:26 -0300

    first-commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace Banco_de_dados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.consulta = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.consultaN = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(54, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Novo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 321);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Salvar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(51, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Registro";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(51, 146);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 201);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Endereço:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(51, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Telefone:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(126, 256);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(126, 198);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(163, 20);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(126, 146);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(163, 20);
+            this.textBox3.TabIndex = 8;
+            // 
+            // consulta
+            // 
+            this.consulta.Location = new System.Drawing.Point(362, 386);
+            this.consulta.Name = "consulta";
+            this.consulta.Size = new System.Drawing.Size(43, 20);
+            this.consulta.TabIndex = 9;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(54, 383);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Limpar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(271, 383);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "NR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(123, 71);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 12;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(170, 384);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 13;
+            this.button5.Text = "Deletar";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // consultaN
+            // 
+            this.consultaN.Location = new System.Drawing.Point(503, 256);
+            this.consultaN.Name = "consultaN";
+            this.consultaN.Size = new System.Drawing.Size(184, 20);
+            this.consultaN.TabIndex = 15;
+            this.consultaN.KeyUp += new System.Windows.Forms.KeyEventHandler(this.consultaN_KeyUp);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(503, 285);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 134);
+            this.listBox1.TabIndex = 16;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(399, 259);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Consulta Por Nome: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.consultaN);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.consulta);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox consulta;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox consultaN;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Banco_de_dados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        RichTextBox rbt = new RichTextBox();
+        int nr;
+        string[] campo;
+        private void Limpar()
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            consulta.Clear();
+            listBox1.Items.Clear();
+
+            
+        }
+        private void Alterar(int nr, string nome, string ender, string fone, string flag)
+        {
+            string linhanew = nome + "|" + ender + "|" + fone + "|" + flag;
+            string oldlinha = rbt.Lines[nr];
+            rbt.Text = rbt.Text.Replace(oldlinha, linhanew);
+            rbt.SaveFile("clientes.cly", RichTextBoxStreamType.PlainText);
+            rbt.LoadFile("clientes.cly", RichTextBoxStreamType.PlainText);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                nr = int.Parse(consulta.Text);
+                campo = rbt.Lines[nr].Split('|');
+                if (campo[3] == "D")
+                {
+                    MessageBox.Show("Registro deletado", "Erro ao procurar Registro");
+                    Limpar();
+                }
+                else
+                {
+                    textBox3.Text = campo[0];
+                    textBox2.Text = campo[1];
+                    textBox1.Text = campo[2];
+                    label5.Text = nr.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Digite algo no campo de consulta", "Erro de Burrice");
+            }
+            
+        }
+        
+        
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                if (label5.Text == consulta.Text)
+                {
+                    Alterar(nr, textBox3.Text, textBox2.Text, textBox1.Text, "A");
+
+                }
+                else
+                {
+                    rbt.Text += "\n" + textBox3.Text + "|" + textBox2.Text + "|" + textBox1.Text + "|" + "A";
+                    rbt.SaveFile("clientes.cly", RichTextBoxStreamType.PlainText);
+                    MessageBox.Show("Salvo com sucesso ");
+                   
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Campo vazio, verifique e tente novamente", "Erro");
+                textBox1.Focus();
+            }
+            
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Limpar();
+            label5.Text = rbt.Lines.Length.ToString();
+            rbt.LoadFile("clientes.cly", RichTextBoxStreamType.PlainText);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Limpar();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                rbt.LoadFile("clientes.cly", RichTextBoxStreamType.PlainText);
+                label5.Text = rbt.Lines.Length.ToString();
+              
+            }
+            catch (IOException IOex)
+            {
+                rbt.SaveFile("clientes.cly", RichTextBoxStreamType.PlainText);
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if ( (e.KeyChar < '0' || e.KeyChar > '9') && (e.KeyChar != (char) 8) && (e.KeyChar!= (char) 127) && (e.KeyChar!='-')
+                && (e.KeyChar!='(') && (e.KeyChar != ')'))
+            {
+                e.KeyChar = (char)0;
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+          
+                if (nr.ToString() == label5.Text)
+                {
+                    if (MessageBox.Show("Deseja excluir esse registro", "Atenção", MessageBoxButtons.OKCancel) == DialogResult.OK)
+                     {
+                        Alterar(nr, textBox3.Text, textBox2.Text, textBox1.Text, "D");
+                    MessageBox.Show("Resgistro deletado com sucesso", "DLETADO");
+                    Limpar();
+
+                }
+                }
+                else
+                {
+                MessageBox.Show("Escolha um registro para excluir", "Resgitro não selecionado");
+                }
+     
+          
+        }
+
+        int i;
+        
+        string[] letras;
+
+        private void consultaN_KeyUp(object sender, KeyEventArgs e)
+        {
+            Limpar();
+            for (i = 1; i < rbt.Lines.Length; i++)
+            {
+
+                letras = rbt.Lines[i].Split('|');
+
+                if (letras[0].Substring(0, consultaN.Text.Length).ToUpper() == consultaN.Text.ToUpper())
+                {
+
+                    listBox1.Items.Add(letras[0].Substring(0));
+
+                    
+                }
+
+            }
+        }
+
+    
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            string nome = listBox1.SelectedItem.ToString(); //listBox1.Items[it].ToString();
+
+            for (i = 1; i < rbt.Lines.Length; i++)
+            {
+
+
+                letras = rbt.Lines[i].Split('|');
+
+
+                if (letras[0].ToUpper() == nome.ToUpper())
+                {
+                    textBox3.Text = letras[0];
+                    textBox2.Text = letras[1];
+                    textBox1.Text = letras[2];
+                    label5.Text = i.ToString();
+
+                }
+            }
+        }
+    }
+}
